51503e1e9b5acd5e64c71ff966c75fccaf6ff0e7
GiL <iamleeyounggil@gmail.com>
2017-10-26 15:44:07 -0400

    Implement sending email using sendgrid pkg and send email to my email for testing successfully

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 
 const Survey = mongoose.model('surveys');
 
@@ -17,5 +20,8 @@ module.exports = app => {
             dateSent: Date.now()
 
         });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 }
