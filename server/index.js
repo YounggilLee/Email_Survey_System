db8af6c8796c68766e78009e4376b2fe343f7b97
GiL <iamleeyounggil@gmail.com>
2017-11-03 14:05:29 -0400

    Create sendgrid_webhook to fix server issue

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
+//mongoose.connect(keys.mongoURI, {useMongoClient:true});
 mongoose.connect(keys.mongoURI);
 
 const app = express();
